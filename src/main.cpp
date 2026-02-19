1a11ef5e151d4b9ef45fa4a4c8aeb618ed15bd2c
Algimantas20 <algsamalionis@gmail.com>
2026-02-19 15:00:27 +0200

    Added temperature reading from TMP102

--- src/main.cpp
+++ src/main.cpp
@@ -1,19 +1,45 @@
 #include "Config.hpp"
 #include <Arduino.h>
+#include <Wire.h>          // For I2C communication
+
+#define TMP102_ADDR 0x48   // Default if ADD0 is GND
+
+void get_temp()
+{
+    Wire.beginTransmission(TMP102_ADDR);
+    Wire.write(0x00); 
+    Wire.endTransmission();
+
+    Wire.requestFrom(TMP102_ADDR, 2);
+
+    if (Wire.available() != 2) 
+    {
+        Serial.println("Error reading from TMP102");
+        return;
+    }
+    
+    uint8_t msb = Wire.read();
+    uint8_t lsb = Wire.read();
+    int16_t temp_raw = ((msb << 8) | lsb) >> 4; // 12-bit value
+    float tempC = temp_raw * 0.0625;            // Each LSB = 0.0625°C
+
+    Serial.print("Temperature: ");
+    Serial.print(tempC);
+    Serial.println(" °C");
+
+}
 
 void setup() 
 {
-  pinMode(Config::LED_PIN, OUTPUT);
-  Serial.begin(Config::UPLOAD_RATE);
-  Serial.println("Hello, world! (Setup)");
+    Serial.begin(115200);
+    Wire.begin(21, 22);   // TMP102 SDA = GPIO21, SCL = GPIO22
+
+    Serial.println("Heimdall is running...");
 }
 
 void loop() 
 {
-  digitalWrite(Config::LED_PIN, HIGH);
-  Serial.println("Set to HIGH!");
-  delay(1000);
-  digitalWrite(Config::LED_PIN, LOW);
-  Serial.println("Set to LOW!");
-  delay(1000);
+    get_temp();
+
+    delay(1000);
 }
